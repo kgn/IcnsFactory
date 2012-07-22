06bf98129fc055cb725e88a59ab0f569dea3f452
David Keegan <git@davidkeegan.com>
2012-07-22 14:22:59 -0700

    Support for 1024, 512, 256

--- IcnsFactory.m
+++ IcnsFactory.m
@@ -21,42 +21,54 @@ UInt32 flipUInt32(UInt32 littleEndian){
 
 @implementation IcnsFactory
 
++ (NSDictionary *)iconSizes{
+    static dispatch_once_t once;
+    static NSDictionary *iconSizes;
+    dispatch_once(&once, ^{
+        iconSizes = @{@1024: @"ic10", @512: @"ic09", @256: @"ic08"};
+    });
+    return iconSizes;
+}
+
 + (BOOL)writeICNSToFile:(NSString *)filePath withImages:(NSArray *)images{
-	if(![[NSFileManager defaultManager] fileExistsAtPath:filePath]){
-		[[NSFileManager defaultManager] createFileAtPath:filePath contents:[NSData data] attributes:nil];
+	if([[NSFileManager defaultManager] fileExistsAtPath:filePath]){
+        [[NSFileManager defaultManager] removeItemAtPath:filePath error:nil];
 	}
+    [[NSFileManager defaultManager] createFileAtPath:filePath contents:[NSData data] attributes:nil];    
 	NSFileHandle *handle = [NSFileHandle fileHandleForWritingAtPath:filePath];
 	if(handle == nil){
 		return NO;
 	}
     
-    NSMutableData *headerData = [NSMutableData data];
-    NSMutableData *bodyData = [NSMutableData data];
-    [images enumerateObjectsUsingBlock:^(NSImage *image, NSUInteger idx, BOOL *stop) {
+    __block NSUInteger bodyLength = 0;
+    __block NSMutableData *bodyData = [NSMutableData data];
+    [images enumerateObjectsUsingBlock:^(NSImage *image, NSUInteger idx, BOOL *stop){
         NSUInteger width = round(image.size.width);
         NSUInteger height = round(image.size.height);
-        if(width == height && height == 512){
-            NSDictionary *properties = [NSDictionary dictionaryWithObject:[NSNumber numberWithFloat:1.0] forKey:NSImageCompressionFactor];
-            NSBitmapImageRep *rep = [[NSBitmapImageRep alloc] initWithData:[image TIFFRepresentation]];
-            NSData *jpeg2000Data = [rep representationUsingType:NSJPEG2000FileType properties:properties];
-            
-            // header
-            UInt32 fileLength = flipUInt32(16 + (UInt32)[jpeg2000Data length]);
-            [headerData appendData:[@"icns" dataUsingEncoding:NSASCIIStringEncoding]];
-            [headerData appendBytes:&fileLength length:4];
-            
-            // body
-            UInt32 elemLength = flipUInt32(8 + (UInt32)[jpeg2000Data length]);
-            NSString *elemName = width == 256 ? @"ic08" : @"ic09";
-            [bodyData appendData:[elemName dataUsingEncoding:NSASCIIStringEncoding]];
-            [bodyData appendBytes:&elemLength length:4];
-            [bodyData appendData:jpeg2000Data];            
+        if(width != height){
+            return;
         }
+        NSString *type = [[self iconSizes] objectForKey:[NSNumber numberWithInteger:round(image.size.width)]];
+        if(type == nil){
+            return;
+        }
+        NSBitmapImageRep *bitmap = [[NSBitmapImageRep alloc] initWithData:[image TIFFRepresentation]];
+        NSData *pngData = [bitmap representationUsingType:NSPNGFileType properties:nil];
+        bodyLength += [pngData length];
+        
+        UInt32 length = flipUInt32(8 + (UInt32)[pngData length]);
+        [bodyData appendData:[type dataUsingEncoding:NSASCIIStringEncoding]];
+        [bodyData appendBytes:&length length:4];
+        [bodyData appendData:pngData];
     }];
+    
+    NSMutableData *headerData = [NSMutableData data];
+    UInt32 fileLength = flipUInt32(16 + (UInt32)bodyLength);
+    [headerData appendData:[@"icns" dataUsingEncoding:NSASCIIStringEncoding]];
+    [headerData appendBytes:&fileLength length:4];
 	[handle writeData:headerData];
 	[handle writeData:bodyData];
 	[handle closeFile];
-    NSLog(@"%@", filePath);
     return YES;
 }
 

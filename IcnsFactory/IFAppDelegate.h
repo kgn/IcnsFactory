91e3ebecfab28aff1013eeb479a8547753582b6a
David Keegan <git@davidkeegan.com>
2012-07-22 12:04:56 -0700

    Initial Commit

---
+++ IcnsFactory/IFAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  IFAppDelegate.h
+//  IcnsFactory
+//
+//  Created by David Keegan on 7/22/12.
+//  Copyright (c) 2012 David Keegan. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface IFAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ IcnsFactory/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  IcnsFactory
+//
+//  Created by David Keegan on 7/22/12.
+//  Copyright (c) 2012 David Keegan. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
